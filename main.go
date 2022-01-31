97a9640ace5ef030882364a57d2aeaca1f1eb203
Hary <phdhary@gmail.com>
2022-02-01 06:23:39 +0700

    string using backtick

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var message string = "Hello"
+	var message string = `My name is "Oliver Queen".
+	I am the arrow`
 	// %t for bool format to string
 	fmt.Printf("message: %s \n", message)
 }
