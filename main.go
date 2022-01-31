d943098408e8326db9031a853b2af131f11c68fe
Hary <phdhary@gmail.com>
2022-02-01 06:34:24 +0700

    ifelse temp variable

--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var point = 8
+	var point = 10000.0
 
-	if point == 10 {
-		fmt.Println("Passed perfectly")
-	} else if point > 5 {
-		fmt.Println("Passed")
-	} else if point == 4 {
-		fmt.Println("Almost there")
+	if percent := point / 100; percent >= 100 {
+		fmt.Printf("%.1f%s Passed perfectly", percent, "%")
+	} else if point >= 70 {
+		fmt.Printf("%.1f%s Good", percent, "%")
 	} else {
-		fmt.Printf("You can do better! Your score is %d\n", point)
+		fmt.Printf("You can do better! Your score is %f\n", point)
 	}
 }
