2cb8f78233ba698e8f893d3985c1f77d3bb55638
Hary <phdhary@gmail.com>
2022-01-31 22:44:16 +0700

    bool

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var decimalNumber = 2.62
-	// %f to format decimal to string
-	// %3.f to format 3 digit decimal to string
-	fmt.Printf("decimal num: %f\n", decimalNumber)
-	fmt.Printf("decimal num: %.3f\n", decimalNumber)
+	var exist bool = true
+	// %t for bool format to string
+	fmt.Printf("exist? %t \n", exist)
 }
