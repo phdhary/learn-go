8b456e691d6fc8323088249e10810752b9760d58
Hary <phdhary@gmail.com>
2022-01-31 22:42:54 +0700

    float/decimal

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var positiveNumber uint8 = 89
-	var negativeNumber = -12345678
-
-	fmt.Printf("positive num: %d\n", positiveNumber)
-	fmt.Printf("negative num: %d\n", negativeNumber)
+	var decimalNumber = 2.62
+	// %f to format decimal to string
+	// %3.f to format 3 digit decimal to string
+	fmt.Printf("decimal num: %f\n", decimalNumber)
+	fmt.Printf("decimal num: %.3f\n", decimalNumber)
 }
