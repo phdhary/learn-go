daac9abff08be8ce3c00e9bb1c0d0fbd7ec68b03
Hary <phdhary@gmail.com>
2022-02-01 06:59:25 +0700

    array

--- main.go
+++ main.go
@@ -5,14 +5,12 @@ import (
 )
 
 func main() {
+	var names [4]string
 
-outerLoop:
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if i == 3 {
-				break outerLoop
-			}
-			fmt.Print("matrix [", i, "][", j, "]", "\n")
-		}
-	}
+	names[0] = "john"
+	names[1] = "deto"
+	names[2] = "berto"
+	names[3] = "bernado"
+
+	fmt.Println(names, names[3])
 }
