bc22f5863cd973d8ff1a9bde79d696820e6de01f
Hary <phdhary@gmail.com>
2022-01-31 22:30:04 +0700

    var underscore

--- main.go
+++ main.go
@@ -9,5 +9,11 @@ func main() {
 
 	lastName := "Chagreta"
 
+	_ = "this is variable underscore that will be ignored"
+	_ = "this is variable underscore that will be ignored too"
+
+	fruit, _ := "apple", "grape"
+
 	fmt.Printf("Hello %s %s!\n", firstName, lastName)
+	fmt.Print(fruit)
 }
