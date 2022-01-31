16341d829d5916febd296565c5fa0768a5d23270
Hary <phdhary@gmail.com>
2022-01-31 13:55:35 +0700

    create hello with http

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, world")
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Hello, this is the result of your request, your requested address:  %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":1290", nil)
 }
