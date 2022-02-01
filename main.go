16572621583b8bd2bdaf8f4b35d5fd9f062b0135
Hary <phdhary@gmail.com>
2022-02-01 11:18:08 +0700

    pointer with parameter

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
-	var numberA int = 4
-	var numberB *int = &numberA
+	var number = 4
+	fmt.Println("before: ", number)
 
-	fmt.Println("numberA (value)    :", numberA)
-	fmt.Println("numberA (address)    :", &numberA)
-	fmt.Println("numberB (value)    :", *numberB)
-	fmt.Println("numberB (address)    :", numberB)
+	change(&number, 50)
+	fmt.Println("after: ", number)
+
+}
+
+func change(original *int, value int) {
+	*original = value
 }
